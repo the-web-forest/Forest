75804dd2cc371d7860f3bfb2009de50e830b66a8
Barros <mdbf42@gmail.com>
2022-06-20 22:35:59 -0300

    Route Created -Get User Info

---
+++ Ipe/UseCases/GetUserInfo/GetUserInfoUseCase.cs
@@ -0,0 +1,28 @@
+﻿using Ipe.UseCases.Interfaces;
+
+namespace Ipe.UseCases.GetUserInfo
+{
+    public class GetUserInfoUseCase : IUseCase<GetUserInfoUseCaseInput, GetUserInfoUseCaseOutput>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserInfoUseCase(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<GetUserInfoUseCaseOutput> Run(GetUserInfoUseCaseInput Input)
+        {
+            var User = await _userRepository.GetById(Input.UserId);
+
+            return new GetUserInfoUseCaseOutput {
+                Name = User.Name,
+                Email = User.Email,
+                City = User.City,
+                State = User.State
+            };
+           
+        }
+
+    }
+}
